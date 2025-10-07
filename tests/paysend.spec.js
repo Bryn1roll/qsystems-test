34c985b5ab0ef6ca1663fe99b19d14943f67e670
Vladislav Golban <155025561+Bryn1roll@users.noreply.github.com>
2025-10-07 16:43:31 +0300

    Paysend playwright tests

---
+++ tests/paysend.spec.js
@@ -0,0 +1,56 @@
+// данные автотесты иммитируют поведение пользователя на сайте paysend.com
+// вводят сумму для отправки, получают сумму получателя, комиссию и курс конвертации
+// также я добавил проверку корректности конвертации с учетом комиссии, т.к считаю это критически важным
+// для подобных сайтов. Автотесты настроены на запуск в Chromium, 
+// при необходимости можно раскомментировать другие браузеры в playwright.config.js и запустить в них.
+// Но делать этого я не советую, т.к у сайта присутствует защита cloudflare
+
+import { test, expect } from '@playwright/test';
+import { exchangePage } from '../pageobject/exchangepage';
+
+test('euro-to-gbp', async ({ page }) => {
+  const exchange = new exchangePage(page);
+  await page.goto(`${process.env.LANDING_URL}/from-ireland-to-united-kingdom`);
+  const data = await exchange.getPageData('1000.00');
+  await exchange.checkConversion(data);
+
+
+});
+
+test('gbp-to-euro', async ({ page }) => {
+  const exchange = new exchangePage(page);
+  await page.goto(`${process.env.LANDING_URL}/from-united-kingdom-to-belgium`);
+  const data = await exchange.getPageData('1000.00');
+  await exchange.checkConversion(data);
+
+});
+
+test('eur-to-chf', async ({ page }) => {
+  const exchange = new exchangePage(page);
+  await page.goto(`${process.env.LANDING_URL}/from-austria-to-switzerland`);
+  const data = await exchange.getPageData('1000.00');
+  await exchange.checkConversion(data);
+
+});
+
+test('aud-to-cad', async ({ page }) => {
+  const exchange = new exchangePage(page);
+  await page.goto(`${process.env.LANDING_URL}/from-australia-to-canada`);
+  const data = await exchange.getPageData('1000.00');
+  await exchange.checkConversion(data);
+
+});
+
+test('aud-to-cny', async ({ page }) => {
+  const exchange = new exchangePage(page);
+  await page.goto(`${process.env.LANDING_URL}/from-australia-to-china`);
+  const data = await exchange.getPageData('1000.00');
+  await exchange.checkConversion(data);
+
+});
+
+test.afterEach(async ({ page }) => {
+  await page.close();
+});
+
+
